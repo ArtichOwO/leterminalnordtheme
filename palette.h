e4701313ee192b9e59dd0d69464ea35a8a24b416
ArtichautCosmique <63865385+ArtichOwO@users.noreply.github.com>
2021-02-09 16:17:00 +0100

    Added palette.h

---
+++ palette.h
@@ -0,0 +1,15 @@
+#ifndef TERMINAL_COLOR_PALETTE
+#define TERMINAL_COLOR_PALETTE
+#include <kandinsky.h>
+
+constexpr KDColor TerminalBackground = KDColor::RGB24(0x2E3440);
+constexpr KDColor TerminalWhite = KDColorWhite;
+constexpr KDColor TerminalRed = KDColor::RGB24(0xBF616A);
+constexpr KDColor TerminalGreen = KDColor::RGB24(0xA3BE8C);
+constexpr KDColor TerminalBlue = KDColor::RGB24(0x5E81AC);
+constexpr KDColor TerminalYellow = KDColor::RGB24(0xEBCB8B);
+constexpr KDColor TerminalBeige = KDColor::RGB24(0xD08770);
+constexpr KDColor TerminalIndigo = KDColor::RGB24(0xB48EAD);
+constexpr KDColor TerminalPink = KDColor::RGB24(0xF88EAD);
+
+#endif
